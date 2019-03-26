baa5bc31f4343a9d8775671dcc32fbd73fcb29dc
Thiago Almeida <thiago9864@hotmail.com>
2019-03-26 20:22:41 -0300

    Projeto do trabalho criado
    Projeto criado no CodeBlocks

---
+++ membros/thiago/Trabalho/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+
+Trabalho Grafos 2019.1
+Grupo 7
+Tema: <a escolher>
+Integrantes: Christian, Laura, Gabriele, Thiago, Victor
+
+**/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
