be624b4742f372823f8c830a3e25956785f3940d
Thiago Almeida <thiago9864@hotmail.com>
2019-03-26 23:48:08 -0300

    Update Thiago

--- membros/thiago/Trabalho/main.cpp
+++ membros/thiago/Trabalho/main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
+#include <string>
+#include "FileManager.h"
 
 using namespace std;
 
 /**
+Prototipo de codigo pra avaliacao
 
-Trabalho Grafos 2019.1
-Grupo 7
-Tema: <a escolher>
-Integrantes: Christian, Laura, Gabriele, Thiago, Victor
+LINHA DE COMANDO PRA RODAR: 
+g++ main.cpp -o main && ./main entrada.txt saida.txt
 
 **/
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
+    string arquivoEntrada, arquivoSaida;
+    //verifica a quantidade de parametros recebidos
+    /*
+    if(argc < 3){
+        cout << "Numero de parametros insuficiente. use Trabalho <arq entrada> <arq saida>" << endl;
+        return 0;
+    }
+
+    //parametros obrigatorios
+    arquivoEntrada = argv[1];
+    arquivoSaida = argv[2];
+    */
+
+    arquivoEntrada = "entrada.txt";
+    arquivoSaida = "saida.txt";
+
+    FileManager fs(arquivoEntrada);
+    cout << "Arq Entrada: " << fs.abrir() << endl;
+    cout << "Arq Saida: " << arquivoSaida << endl;
+
     return 0;
 }
