b5c831b9dfb5ab2f4aa15447c12d3eb5cb0ac72f
Thiago Almeida <thiago9864@hotmail.com>
2019-04-09 00:51:00 -0300

    Atividade 1

---
+++ atividades/1_lista_adjacencia/Grafo.cpp
@@ -0,0 +1,361 @@
+//
+// Created by victor on 01/04/19.
+//
+
+#include <iostream>
+#include "Grafo.h"
+
+using namespace std;
+
+
+Grafo::Grafo() {
+    this->direcional = false;
+    this->ponderadoAresta = false;
+    this->ponderadoNo = false;
+}
+
+Grafo::Grafo(string entrada, string saida) {
+    this->direcional = false;
+    this->ponderadoAresta = false;
+    this->ponderadoNo = false;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
+}
+
+Grafo::Grafo(string entrada, string saida, bool direcional) {
+    this->direcional = direcional;
+    this->ponderadoNo = false;
+    this->ponderadoAresta = false;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
+}
+
+Grafo::Grafo(string entrada, string saida, bool direcional, bool ponderadoAresta) {
+    this->direcional = direcional;
+    this->ponderadoNo = false;
+    this->ponderadoAresta = ponderadoAresta;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
+}
+
+Grafo::Grafo(string entrada, string saida, bool direcional, bool ponderadoNo, bool ponderadoAresta) {
+    this->direcional = direcional;
+    this->ponderadoNo = ponderadoNo;
+    this->ponderadoAresta = ponderadoAresta;
+    this->leitura_arquivo(entrada);
+    this->imprime(saida);
+}
+
+Grafo::~Grafo() {
+    delete this->listaNos;
+}
+
+
+// *** PUBLIC ***
+
+/**
+     * Imprime na tela grafo no formato de lista de adjacência:
+     *
+     * DADOS:
+     * Grau: valor
+     * Ordem: valor
+     * M: valor
+     *
+     * LISTA DE ADJACENCIA:
+     * Vertice de origem --> Vertice adjacente (Peso) --> ...
+     */
+void Grafo::imprime() {
+    cout << "DADOS: " << endl;
+    cout << "Grau: " << this->grau << endl;
+    cout << "Ordem: " << this->ordem << endl;
+    cout << "M: " << this->m << endl;
+
+    cout << "LISTA DE ADJACENCIA: " << endl;
+    for (No* n = this->listaNos; n != nullptr; n = n->getProx()) {
+
+        cout << n->getId() << " ---> ";
+
+        for (Aresta *a = n->getAresta(); a != nullptr; a = a->getProx()) {
+            // Se for ponderado nas arestas é imprimido o peso
+            if (ponderadoAresta) {
+                if (a->getProx() != nullptr)
+                    cout << a->getNoAdj() << " (" << a->getPeso() << ") ---> ";
+                else
+                    cout << a->getNoAdj() << " (" << a->getPeso() << ")";
+            } else {
+                if (a->getProx() != nullptr)
+                    cout << a->getNoAdj() << " ---> ";
+                else
+                    cout << a->getNoAdj();
+            }
+        }
+
+        cout << endl;
+
+    }
+}
+
+/**
+     * Imprime na tela grafo no formato de lista de adjacência:
+     *
+     * DADOS:
+     * Grau: valor
+     * Ordem: valor
+     * M: valor
+     *
+     * LISTA DE ADJACENCIA:
+     * Vertice de origem --> Vertice adjacente (Peso) --> ...
+     *
+     * @param arquivo: Caminho do arquivo onde será gravada a impressão
+     */
+void Grafo::imprime(string arquivo) {
+
+    ofstream file;
+
+    file.open(arquivo, ios::out);
+
+    if (file.is_open()) {
+
+        file << "DADOS: " << endl;
+        file << "Grau: " << this->grau << endl;
+        file << "Ordem: " << this->ordem << endl;
+        file << "M: " << this->m << endl;
+
+        file << "LISTA DE ADJACENCIA: " << endl;
+        for (No *n = this->listaNos; n != nullptr; n = n->getProx()) {
+
+            file << n->getId() << " ---> ";
+
+            for (Aresta *a = n->getAresta(); a != nullptr; a = a->getProx()) {
+                // Se for ponderado nas arestas é imprimido o peso
+                if (ponderadoAresta) {
+                    if (a->getProx() != nullptr)
+                        file << a->getNoAdj() << " (" << a->getPeso() << ") ---> ";
+                    else
+                        file << a->getNoAdj() << " (" << a->getPeso() << ")";
+                } else {
+                    if (a->getProx() != nullptr)
+                        file << a->getNoAdj() << " ---> ";
+                    else
+                        file << a->getNoAdj();
+                }
+            }
+
+            file << endl;
+
+        }
+    } else {
+        cout << "Output file does not open." << endl;
+    }
+}
+
+// *** GETTERS E SETTERS ***
+
+
+void Grafo::setNo(int id, float peso) {
+
+    No* no = new No(id, peso);
+
+    No *list = this->listaNos;
+
+    if (list == nullptr) {
+        this->listaNos = no;
+        this->ordem = 1; // Inicializa a ordem
+        return;
+    }
+
+    // Percorrendo lista de nós até encontrar o ultimo
+    for (list; list->getProx() != nullptr; list = list->getProx()) {
+        if (list->getId() == no->getId()) {
+            //cout << "No com mesmo id ja inserido" << endl;
+            return;
+        }
+    }
+
+    // Certificando se o nó já não foi inserido
+    if (list->getId() != no->getId()) {
+        list->setProx(no);
+        this->ordem++; // Aumentando a ordem
+    }
+
+}
+
+void Grafo::setNo(int id) {
+    this->setNo(id, -1);
+}
+
+void Grafo::setAresta(int idOrigem, int idFim, float peso) {
+    No * list = this->listaNos;
+    No * origem = nullptr;
+    No * fim = nullptr;
+
+    // Evitando self loops
+    if (idOrigem == idFim) {
+        cout << "Nao e permitido self-loop" << endl;
+        return;
+    }
+
+    // Percorrendo lista de nós até encontrar os nós que irão receber a aresta
+    for (list; list != nullptr; list = list->getProx()) {
+
+        if (list->getId() == idOrigem)
+            origem = list;
+
+        if (list->getId() == idFim)
+            fim = list;
+
+        // Evitando percorrer até o final desnecessáriamente
+        if (origem != nullptr && fim != nullptr) break;
+
+    }
+
+    if (origem != nullptr && fim != nullptr) {
+        Aresta *origem_fim = new Aresta(idFim, peso);
+
+        if (origem->setAresta(origem_fim))
+            fim->aumentaGrauEntrada();
+
+    } else {
+        cout << "No nao encontrado" << endl;
+        return;
+    }
+
+    // Se o grafo não for direcional o nó de chegada também recebe uma aresta
+    if (!this->direcional) {
+        Aresta *fim_origem = new Aresta(idOrigem, peso);
+
+        if (fim->setAresta(fim_origem))
+            origem->aumentaGrauEntrada();
+
+        // Atualizando o grau do grafo
+
+        // Como ambos graus são atualizados para grafos não direcionais
+        // Tanto faz verifivar o grau de saída ou entrada;
+        int grauOrigem = origem->getGrauEntrada();
+        int grauFim = fim->getGrauEntrada();
+
+        if (grauOrigem > grauFim)
+            this->atualizaGrau(grauOrigem);
+        else
+            this->atualizaGrau(grauFim);
+
+    }
+
+    // Atualizando grau do grafo
+
+    int grauOrigem = origem->getGrauSaida();
+    int grauFim = fim->getGrauEntrada();
+
+    if (grauOrigem > grauFim)
+        this->atualizaGrau(grauOrigem);
+    else
+        this->atualizaGrau(grauFim);
+
+    this->m++;
+
+}
+
+void Grafo::setAresta(int idOrigem, int idFim) {
+    // Verifica se o grafo é ponderado ou não da aresta para colocar um valour default
+    if (!this->ponderadoAresta)
+        this->setAresta(idOrigem, idFim, -1);
+    else
+        this->setAresta(idOrigem, idFim, 1);
+
+}
+
+
+No* Grafo::getNo(int id) {
+    No *n;
+
+    // Percorrendo lista de nós até encontrar o desejado
+    for (n = this->listaNos; n != nullptr && n->getId() != id; n = n->getProx());
+
+    return n;
+}
+
+Aresta* Grafo::getAresta(int idOrigem, int idFim) {
+    No *n = this->getNo(idOrigem); // Encontrando nó de origem
+
+    // Se encontrar nó inicia a busca se não retorna nullptr
+    if (n != nullptr) {
+        Aresta* a = n->getAresta();
+
+        // Procurando aresta com nó passado, se encontrar o retorna, se não retorna nullptr
+        for (a; a != nullptr && a->getNoAdj() != idFim; a = a->getProx());
+
+        return a;
+
+    } else {
+        return nullptr;
+    }
+
+}
+
+// *** PRIVATE ***
+
+void Grafo::atualizaGrau(int grau) {
+    if (grau > this->grau)
+        this->grau = grau;
+}
+
+void Grafo::leitura_arquivo(string arquivo) {
+
+    ifstream file; // Leitura de arquivo
+    int n_nos = 0; // Numero de nós
+    string line; // Auxilar para leitura de cada linha
+
+    file.open(arquivo, ios::in); // Lendo arquivo de entrada
+
+    if (file.is_open()) {
+
+        getline(file, line); // Lendo a primeira linha com número de nós
+
+        n_nos = stoi(line); // Convertendo de string para int
+
+        //this->ordem = n_nos;
+
+        // Percorrendo cada linha do arquivo e adicionando a variavel dados
+        while (getline(file, line)) {
+
+            istringstream str_stream(line);
+
+            int origem, fim;
+            float peso;
+
+            // Verifica se é ponderado nas arestas
+            if (this->ponderadoAresta) {
+
+                // Lendo valores
+                str_stream >> origem >> fim >> peso;
+
+                // TODO implementar pensando que pode ser ponderado no nó
+
+                // Inserindo nós da aresta, a verificaçao de existencia é feita pelo método
+                this->setNo(origem);
+                this->setNo(fim);
+                this->setAresta(origem,fim,peso);
+
+            } else {
+
+                // Lendo valores
+                str_stream >> origem >> fim;
+
+                // TODO implementar pensando que pode ser ponderado no nó
+
+                // Inserindo nós da aresta, a verificaçao de existencia é feita pelo método
+                this->setNo(origem);
+                this->setNo(fim);
+                this->setAresta(origem,fim);
+
+            }
+
+        }
+
+        file.close();
+
+    } else {
+        cout << "Input file does not open." << endl;
+    }
+}
+
