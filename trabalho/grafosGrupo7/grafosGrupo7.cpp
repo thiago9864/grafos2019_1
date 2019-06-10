7d780b893cab3f5951d86489898ecb5e328323a9
Thiago Almeida <thiago9864@hotmail.com>
2019-06-10 17:32:18 -0300

    Auto stash before merge of "master" and "origin/master"

--- trabalho/grafosGrupo7/grafosGrupo7.cpp
+++ trabalho/grafosGrupo7/grafosGrupo7.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <string>
-#include "Grafo.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+//#include "Grafo.h"
 
 /* 
 Linha de comando pra rodar
 
 *** txt ***
-UNIX (macOS / Linux): clear && g++ -std=c++11 *.cpp -o main && ./main ../data/entrada2.txt ../data/saida.txt 0 0 1
-WINDOWS: cls & g++ -std=c++11 *.cpp -o main & main.exe ../data/entrada2.txt ../data/saida.txt 0 0 1
+UNIX (macOS / Linux): clear && g++ -std=c++11 *.cpp -o grafosGrupo7 && ./grafosGrupo7 ../data/entrada2.txt ../data/saida.txt 0 0 1
+WINDOWS: cls & g++ -std=c++11 *.cpp -o grafosGrupo7 & grafosGrupo7.exe ../data/entrada2.txt ../data/saida.txt 0 0 1
 
 *** stp ***
 UNIX (macOS / Linux): clear && g++ -std=c++11 *.cpp -o grafosGrupo7 && ./grafosGrupo7 ../data/bipe2p.stp ../data/saida.txt 0 0 1
@@ -20,21 +24,49 @@ bipe2p.stp | 5616
 
 */
 
-#include <iostream>
-#include <string>
-#include "Grafo.h"
-
 using namespace std;
+
+/*===============================================*/
+/********* Controle do arquivo de Saida **********/
+/*===============================================*/
+
+
+ofstream arqSaida;
+
+/**
+ * Abre o arquivo de saída para escrita.
+ * @param nomeArqSaida caminho do arquivo de saida
+ */
+void iniciaArquivoSaida(string caminhoArqSaida)
+{
+    //abre arquivo para saida
+    arqSaida.open(caminhoArqSaida.c_str());
+
+    //verifica se o arquivo foi aberto
+    if (!arqSaida || !arqSaida.is_open())
+    {
+        cout << "Impossivel abrir o arquivo de saida para escrita";
+        exit(1); // sai do programa se nao conseguir abrir o arquivo
+    }
+}
+
+/**
+ * Fecha o arquivo de saída para escrita.
+ */
+void fechaArquivoDeSaida(){
+    if(arqSaida.is_open()){
+        arqSaida.close();
+    }    
+}
 
 int main(int argc, char* argv[]) {
 
     if (argc < 3 || argc > 6) {
         cout << "Argumentos passados invalidos";
-
         return 0;
     }
 
-    Grafo *g;
+    //Grafo *g;
 
     //verifica o formato do arquivo
     string arquivoEntrada = argv[1];
@@ -46,6 +78,7 @@ int main(int argc, char* argv[]) {
     } 
 
     // Verificando a quantidade de argumentos passados
+    /*
     if (argc == 6) {
         g = new Grafo(formato, argv[1], argv[2], stoi(argv[3]), stoi(argv[4]), stoi(argv[5]));
     } else if (argc == 5) {
@@ -55,92 +88,39 @@ int main(int argc, char* argv[]) {
     } else {
         g = new Grafo(formato, argv[1], argv[2]);
     }
-
-    //g->imprime();
-
-    //Grafo *p = g->getComplementar();
-
-    //p->imprime();
-
-    //teste 1 da busca em profundidade (Thiago)
-    
-    Aresta *caminho;
-    cout << endl << "**** Busca em Profundidade ****" << endl << endl;
-    
-    caminho = g->buscaEmProfundidade(10, 90);
-    cout << "Procura caminho 10-90." << endl;
-    if(caminho != NULL){
-        cout << "Encontrado." << endl;
-        Aresta *aux = caminho;
-        while(aux != NULL){
-            cout << "Aresta: (" << aux->getNoOrigem() << ", " << aux->getNoAdj() << ") peso: " << aux->getPeso() << endl;
-            aux = aux->getProx();
-        }
-        cout << endl;
-    } else {
-        cout << "Não foi encontrado." << endl;
-    }
-    /*
-    //teste 2 da busca em profundidade (Thiago)
-    caminho = g->buscaEmProfundidade(10, 24);
-    cout << "Procura caminho 10-24." << endl;
-    if(caminho != NULL){
-        cout << "Encontrado." << endl;
-        Aresta *aux = caminho;
-        while(aux != NULL){
-            cout << "Aresta: (" << aux->getNoOrigem() << ", " << aux->getNoAdj() << ") peso: " << aux->getPeso() << endl;
-            aux = aux->getProx();
-        }
-        cout << endl;
-    } else {
-        cout << "Não foi encontrado." << endl;
-    }
-
-    //teste 3 da busca em profundidade (Thiago)
-    //tenta achar um caminho que não existe entre duas componentes conexas
-    caminho = g->buscaEmProfundidade(10, 82);
-    cout << "Procura caminho que não existe entre 10-82." << endl;
-    if(caminho != NULL){
-        cout << "Encontrado." << endl;
-        Aresta *aux = caminho;
-        while(aux != NULL){
-            cout << "Aresta: (" << aux->getNoOrigem() << ", " << aux->getNoAdj() << ") peso: " << aux->getPeso() << endl;
-            aux = aux->getProx();
-        }
-        cout << endl;
-    } else {
-        cout << "Não foi encontrado." << endl;
-    }
-
-    cout << endl << "**** Busca em Largura ****" << endl << endl;
-
-    //teste 1 da busca em largura (Gabriele)
-    Aresta *largura;
-    cout<<"criou aresta e vai entrar em caminho\n";
-    largura=g->caminho_largura(24);
-    cout<<"vai entrar no while main\n";
-    for(Aresta* a = largura; a != NULL; a = a->getProx()) {
-            cout<<"Origem:"<< a->getNoOrigem()<<" ";
-            cout<<"Fim:"<<a->getNoAdj()<<"\n";
-    }
-
-    //teste 1 da cobertura de vertices (thiago)
-    cout << endl << "**** Cobertura de vértices ****" << endl;
-
-    No* cobertura = g->getCoberturaVertices();
-
-    if(cobertura != NULL){
-        No *n = cobertura;
-        cout << "Cobertura encontrada:" << endl;
-        while(n != NULL){
-            cout << n->getId() << " ";
-            n = n->getProx();
-        }
-        cout << endl;
-    } else {
-        cout << "Nenhuma cobertura foi retornada." << endl;
-    }
     */
 
+    //menu da aplicação
+    //inicializa log
+    string arquivoSaida = argv[2];
+    iniciaArquivoSaida(arquivoSaida);
+
+    arqSaida << "**** Trabalho Grafos 2019.1 - Grupo 7 ****" << endl << endl;
+
+    arqSaida << "## Parametros recebidos ##" << endl;
+    arqSaida << "arquivoEntrada: " + arquivoEntrada << endl;
+    arqSaida << "arquivoSaida: " + arquivoSaida << endl;
+    
+    if(stoi(argv[3])){
+        arqSaida << "isDirecionado: Sim" << endl;
+    } else {
+        arqSaida << "isDirecionado: Não" << endl;
+    }
+
+    if(stoi(argv[5])){
+        arqSaida << "isPonderadoVertice: Sim" << endl;
+    } else {
+        arqSaida << "isPonderadoVertice: Não" << endl;
+    }
+
+    if(stoi(argv[4])){
+        arqSaida << "isPonderadoAresta: Sim" << endl;
+    } else {
+        arqSaida << "isPonderadoAresta: Não" << endl;
+    }
+
+    arqSaida << endl;
+
+    fechaArquivoDeSaida();
     return 0;
 }
