a107207bd5e86672918aa33083b4405e2dba0ce4
Thiago Almeida <thiago9864@hotmail.com>
2019-06-30 13:17:37 -0300

    Resultados Construtivo Caminho Minimo

--- membros/gabriele/Parte2/grafosGrupo7/Steiner.cpp
+++ membros/gabriele/Parte2/grafosGrupo7/Steiner.cpp
@@ -9,23 +9,45 @@
 #include <iostream>
 #include "Steiner.h"
 
-
 using namespace std;
 
 
-Steiner::Steiner(int* terminais, int tam_terminais) {
-    this->terminais = terminais;
-    this->tam_terminais = tam_terminais;
+Steiner::Steiner(Grafo *grafo)
+{
+    this->g = grafo;
+    this->terminais = grafo->getTerminais();
+    this->tam_terminais = grafo->getNumTerminais();
 }
 
-int* Steiner::steiner(float alfa, int maxiter) {
+void Steiner::gerarSemente()
+{
+    // seed do random baseado no tempo
+    semente = time(NULL);
+    srand (semente);
+}
+int Steiner::getSemente()
+{
+    return semente;
+}
+void Steiner::setSemente(int semente)
+{
+    this->semente = semente;
+    srand (semente);
+}
+
+/*========================================================================*/
+/*===================== Algoritmo Guloso Randomizado =====================*/
+/*========================================================================*/
+
+
+float Steiner::GulosoRandomizado(float alfa, int maxiter)
+{
     Grafo* steinerSol;
     No** solucao = new No*[this->g->getOrdem()];
     No** solucao_adj = new No*[this->g->getOrdem()];
     int it = 0;
-    float custo;
+    float custo = 0;
     Grafo* recebeKruskal;
-    srand (time(NULL)); // seed do random baseado no tempo
 
     int tam_sol = 0;
     int tam_adj = 0;
@@ -76,8 +98,11 @@ int* Steiner::steiner(float alfa, int maxiter) {
     }
     //retira os nós que não se encontram entre os terminais
     poda(recebeKruskal);
-    return nullptr;
+
+    //retorna o custo da arvore
+    return recebeKruskal->getCusto();
 }
+
 ////adiciona a aresta do nó inserido na solucao no grafo steinerSol
 //void Steiner::colocaAresta(No* inserido, int tam_solucao,No** solucao,Grafo* steinerSol){
 //     float auxMenor=0;
@@ -92,19 +117,6 @@ int* Steiner::steiner(float alfa, int maxiter) {
 //     }
 //     steinerSol->adicionaAresta(origem,1,inserido->getId(),1,auxMenor);
 //}
-
-
-//adiciona a aresta do nó inserido na solucao no grafo steinerSol
-void Steiner::colocaAresta(No* inserido, int tam_solucao,No** solucao,Grafo* steinerSol){
-    int i=0;
-    for( Aresta *aux=inserido->getAresta(); i<tam_solucao; aux=aux->getProx()){
-        if(aux->getNoAdj()==solucao[i]->getId()){
-            steinerSol->adicionaAresta(solucao[i]->getId(),1,inserido->getId(),1,aux->getPeso());
-        }
-        i++;
-    }
-
-}
 
 int Steiner::binarySearch(float a[], int item, int low, int high) {
     if (high <= low)
@@ -191,13 +203,12 @@ int Steiner::atualizaLista(No** adj, int tam, int pos) {
     return tam;
 }
 
-
-
 //Poda da árvore
 //retirar nós de grau 1 que não estejam no caminho entre os terminais
-void Steiner::poda(Grafo* grafo_novo){
+void Steiner::poda(Grafo* grafo_novo)
+{
     //1º passo: fazer um vetor com os terminais
-    //2º passo: procurar nó que tenha grau 1 com a funç~~ao auxpoda, por meio de uma recursao
+    //2º passo: procurar nó que tenha grau 1 com a função auxpoda, por meio de uma recursao
 
     No** term=new No *[this->tam_terminais];
 
@@ -212,6 +223,7 @@ void Steiner::poda(Grafo* grafo_novo){
 }
 void Steiner::auxPoda(Grafo* grafo_novo, No *aux, No *ant)
 {
+    cout << "entrou: " << aux->getId() << endl;
     if(aux->getGrauEntrada()==1){//caso base: encontrar nó de grau 1
         if(aux->get_marcaTerminal() != true){// se nao for terminal remove
              grafo_novo->removeNo(aux->getId());
@@ -221,18 +233,171 @@ void Steiner::auxPoda(Grafo* grafo_novo, No *aux, No *ant)
         Aresta* adjacente=aux->getAresta();
         while(adjacente!=NULL){
             if(ant->getId() != adjacente->getNoAdj()){
-                auxTeste(grafo_novo, grafo_novo->getNo(adjacente->getNoAdj()), aux);
+                auxPoda(grafo_novo, grafo_novo->getNo(adjacente->getNoAdj()), aux);
             }
             adjacente = adjacente->getProx();
         }
 
     }
 
+    cout << "saiu: " << aux->getId() << endl;
     if(aux->getGrauEntrada()==1){
         if(aux->get_marcaTerminal() != true){
              grafo_novo->removeNo(aux->getId());
         }
     }
 
+}
+
+
+/*========================================================================*/
+/*================= Algoritmo Guloso Randomizado Reativo =================*/
+/*========================================================================*/
+
+
+float Steiner::GulosoRandomizadoReativo(int maxiter)
+{
+    int B = 10;//bloco de iteracoes
+    int max_alfas = 100;//quantidade de alfas
+    float *alfas = new float[max_alfas];
+    int cont=0;//iteracoes do bloco
+    float sum_custo=0;
+    float *S = new float[max_alfas];
+    float S_estrela = 0;
+    float *P = new float[max_alfas];
+    int indexAlfa=-1;
+
+    //inicia com solução gulosa
+    S_estrela = GulosoRandomizado(0, 1);
+
+    for (int i=0; i<maxiter; i++)
+    {
+        if(i%B == 0){
+            atualizaProb(alfas, cont, sum_custo, S, S_estrela, P);
+        }
+        indexAlfa = selecionaAlfa(P);
+        S[indexAlfa] = GulosoRandomizado(alfas[indexAlfa], 1);
+
+        if(S[indexAlfa] < S_estrela){
+            S_estrela = S[indexAlfa];
+        }
+
+        atualizaVetores(S, indexAlfa, cont, sum_custo);
+
+    }
+    return 0;
+}
+
+void Steiner::atualizaProb(float *alfas, int cont, float sum_custo, float *S, float S_estrela, float *P)
+{
+
+}
+
+void Steiner::atualizaVetores(float *S, int indexAlfa, int cont, float sum_custo)
+{
+
+}
+
+int Steiner::selecionaAlfa(float *P)
+{
+    return 0;
+}
+
+
+
+/*========================================================================*/
+/*================ Algoritmo Heurística de Caminho Mínimo ================*/
+/*========================================================================*/
+
+
+float Steiner::ConstrutivoHeuristicaCaminhoMinimo()
+{
+    int *S = new int[g->getOrdem()];
+    S[0] = terminais[0];
+    int n_sol = 1;
+
+    int *C = new int[tam_terminais];
+    int n_can = 0;
+    for(int j=1; j<tam_terminais;j++){
+        C[n_can] = terminais[j];
+        n_can++;
+    }
+
+    //processa o floyd uma unica vez
+    cout << "Processando Floyd" << endl;
+    Floyd *floyd = new Floyd(g, g->getMatrizAdj());
+
+    //inicia o grafo solucao
+    Grafo *h = new Grafo(false, true, false);
+
+    cout << "Processando algoritmo" << endl;
+
+    while(n_can > 0){
+        float custo = 99999999999999.9;
+        int id_origem_escolhido=-1;
+        int id_destino_escolhido=-1;
+
+        for(int i=0; i<n_sol;i++){
+            int id_sol = S[i];
+
+            for(int j=0; j<n_can;j++){
+                int id_can = C[j];
+                float peso = floyd->getDistancia(id_sol, id_can);
+
+                //cout << id_sol << " " << id_can << " " << peso << endl;
+                if(peso < custo){
+                    custo = peso;
+                    id_origem_escolhido = id_sol;
+                    id_destino_escolhido = id_can;
+                }
+            }
+
+        }
+
+        if(id_origem_escolhido != -1){
+            Aresta *aux = floyd->getCaminhoAresta(id_origem_escolhido, id_destino_escolhido);
+            while(aux != NULL){
+                //cout << "Add Aresta: (" << aux->getOrigem() << ", " << aux->getNoAdj() << ") " << aux->getPeso() << endl;
+                h->adicionaAresta(aux->getOrigem(), 1, aux->getNoAdj(), 1, aux->getPeso());
+                aux = aux->getProx();
+            }
+            remover(C, &n_can, id_destino_escolhido);
+            adicionar(S, &n_sol, id_destino_escolhido);
+
+            cout << n_sol << " de " << tam_terminais << endl;
+
+        } else {
+            cout << "Steiner::ConstrutivoHeuristicaCaminhoMinimo: O grafo nao e conexo!" << endl;
+            return NULL;
+        }
+
+    }
+
+    return h->getCusto();
+
+}
+
+void Steiner::remover(int *vetor, int *tam, int valor)
+{
+    //encontra
+    int indice = -1;
+    for(int i=0; i<*tam; i++){
+        if(vetor[i] == valor){
+            indice = i;
+            break;
+        }
+    }
+    //desloca
+    if(indice != -1){
+        for(int i=indice+1; i<*tam; i++){
+            vetor[i-1] = vetor[i];
+        }
+    }
+    *tam = *tam - 1;
+}
+void Steiner::adicionar(int *vetor, int *tam, int valor)
+{
+    vetor[*tam] = valor;
+    *tam = *tam + 1;
 }
 
--- membros/gabriele/Parte2/grafosGrupo7/Steiner.h
+++ membros/gabriele/Parte2/grafosGrupo7/Steiner.h
@@ -14,6 +14,8 @@
 #include <sstream>
 #include <time.h>
 #include "Grafo.h"
+#include "Utils.h"
+#include "Floyd.h"
 
 using namespace std;
 
@@ -21,23 +23,41 @@ class Steiner {
 
 public:
 
-    Steiner(int* terminais, int tam_terminais);
+    Steiner(Grafo *grafo);
 
-    int* steiner(float alfa, int maxiter);
+    float GulosoRandomizado(float alfa, int maxiter);
+    float GulosoRandomizadoReativo(int maxiter);
+    float ConstrutivoHeuristicaCaminhoMinimo();
+
+    void gerarSemente();
+    int getSemente();
+    void setSemente(int semente);
 
     void poda(Grafo* grafo_novo);
-    void auxPoda(Grafo* grafo_novo,No* aux);
 
-    void colocaAresta(No* inserido, int tam_solucao,No** solucao,Grafo* steinerSol);
 private:
 
     Grafo* g;
     int *terminais;
     int tam_terminais;
+    int semente;
 
+    //guloso randomizado
     void ordenaAdj(No** adj, No** sol, int tam_adj, int tam_sol);
     int binarySearch(float a[], int item, int low, int high);
     int atualizaLista(No** adj, int tam, int pos);
+    void colocaAresta(No* inserido, int tam_solucao,No** solucao,Grafo* steinerSol);
+
+    void auxPoda(Grafo* grafo_novo, No *aux, No *ant);
+
+    //guloso randomizado reativo
+    void atualizaProb(float *alfas, int cont, float sum_custo, float *S, float S_estrela, float *P);
+    void atualizaVetores(float *S, int indexAlfa, int cont, float sum_custo);
+    int selecionaAlfa(float *P);
+
+    //construtivo heuristica caminho minimo
+    void remover(int *vetor, int *tam, int valor);
+    void adicionar(int *vetor, int *tam, int valor);
 
 };
 
