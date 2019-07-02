d210462d2c6bd8b853f663e33c47feb39b8acedd
Victor Aquiles <victoraqalencar@gmail.com>
2019-07-01 23:00:21 -0300

    Reativo improved

--- trabalho/Final/GrafosGrupo7/Steiner.cpp
+++ trabalho/Final/GrafosGrupo7/Steiner.cpp
@@ -356,17 +356,17 @@ void Steiner::auxPoda(Grafo* grafo_novo, No *aux, No *ant)
 float Steiner::GulosoRandomizadoReativo(int maxiter)
 {
     int B = 10;//bloco de iteracoes
-    int max_alfas = 100;//quantidade de alfas
-    float *alfas = new float[max_alfas];
+    int max_alfas = 15;//quantidade de alfas
+    float alfas[max_alfas];
     int cont=0;//iteracoes do bloco
     float sum_custo=0;
     float S[max_alfas]; // armazena somatório de soluções do alfa
     int S_iter[max_alfas]; // numero de iterações de cada alfa
     float S_estrela = 0;
-    float *P = new float[max_alfas];
+    float P[max_alfas];
     int indexAlfa=-1;
     int it = 0;
-    float fator = 0.5;
+    float fator = 1.5;
 
     //inicia com solução gulosa
     S_estrela = this->GulosoConstrutivo();
@@ -374,11 +374,14 @@ float Steiner::GulosoRandomizadoReativo(int maxiter)
     // inicializando vetores
     for (int i = 0; i < max_alfas; i++) {
         // inicializando soluções de alfa
-        S[i] = 0.0;
+        S[i] = 1.0;
         S_iter[i] = 0;
 
         // inicializando porcentagens
         P[i] = 1.0/max_alfas;
+
+        // inicializando alfas
+        alfas[i] = (float)1.0-i*(1.0/max_alfas);
     }
 
     while (it < maxiter) {
@@ -387,12 +390,13 @@ float Steiner::GulosoRandomizadoReativo(int maxiter)
 
         for (int i = 0; i < B; i++) {
 
-            if (porcentagemAtual >= i/B) {
+            if (porcentagemAtual <= (float)i/(float)B) {
                 indexAlfa++;
                 porcentagemAtual = porcentagemAtual + P[indexAlfa];
                 i--;
                 continue;
             } else {
+
                 float solucaoAtual = this->GulosoRandomizado(alfas[indexAlfa], maxiter);
 
                 if (S_estrela > solucaoAtual) {
@@ -404,20 +408,20 @@ float Steiner::GulosoRandomizadoReativo(int maxiter)
                 S_iter[indexAlfa]++;
 
             }
-            it++;
         }
 
         // Recalculando probabilidades
         float recalculaP[max_alfas];
         float soma = 0;
         for (int k = 0; k < max_alfas; k++) {
-            float aux = S_estrela/(S[k]/S_iter[k]);
+            float aux = (float)S_estrela/((float)S[k]/(float)S_iter[k]);
             recalculaP[k] = pow(aux, fator);
             soma = soma + recalculaP[k];
         }
         for (int k = 0; k < max_alfas; k++) {
             P[k] = recalculaP[k]/soma;
         }
+        it++;
     }
     return S_estrela;
 }
